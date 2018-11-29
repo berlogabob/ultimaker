46776b47d8b89df8a3479c20abe6f107998f8ebc
Andrew Diakov <berloga.bob@gmail.com>
2018-11-29 17:20:54 +0300

    Update ultimaker_conversion.py

--- ultimaker_conversion.py
+++ ultimaker_conversion.py
@@ -5,9 +5,12 @@ p = math.pi
 d = 2.85
 r = d/2
 
+listok = []
+
+
 h = float(input(message))
 h_mm = h * 1000.0
 v_mm = p * r**2 * h_mm
 v_sm = v_mm * 0.001
-
-print(v_sm)
+v_round = round(v_sm)
+print(v_round)
